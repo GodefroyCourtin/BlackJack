53ec0579ea646fae4c543b9068f830d6baf1f768
Godefroy <courtin.godefroy@gmail.com>
2020-09-03 03:42:41 +0200

    finalisation du frontend. quelques bugs à corriger (aces, égalité)

--- croupier.py
+++ croupier.py
@@ -26,61 +26,161 @@ class Croupier:
                 joueur.hand.append(card)
                 self.paquet.remove(card)
 
-		
+
+
+
 	
 if __name__ == "__main__":
 
-    choix_recommencer = "oui"
-    while choix_recommencer == "oui":
+    #choix_recommencer = "oui"
+    #while choix_recommencer == "oui":
         #Tour1
         joueur = Joueur("beber")                  
         croupier = Croupier() 
+        
+
         print("Début de la partie")    
         print("--------------------------")           
-        croupier.distribue(2,joueur) 
-        croupier.distribue(1)                     
+        croupier.distribue(2, joueur) 
+        croupier.distribue(2)                     
         print("la main du joueur est :", joueur.hand)
         print("--------------------------")
         print("la carte du croupier est :", croupier.joueur.hand[0])
-        joueur.calcul_hand()
+        joueur.calcul_hand(joueur)
         print("--------------------------")
         print("le score du joueur est :", joueur.score)
         croupier.joueur.calcul_hand()
+       # print("--------------------------")
+       # print("le score du croupier est :", croupier.joueur.score)
         print("--------------------------")
-        print("le score du croupier est :", croupier.joueur.score)
-
-        print("--------------------------------")
 
         #Tour2
-    
+        choix_carte_supp =""
+
+        while choix_carte_supp != "oui" and choix_carte_supp != "non":
+            choix_carte_supp = input("Recevoir une nouvelle carte ?:")
+
+        if choix_carte_supp == "non":
+
+            print("la main du croupier est :", croupier.joueur.hand)
+            print("--------------------------")  
+            print("le score du croupier est de :", croupier.joueur.score)
+            print("--------------------------")  
+
+            while croupier.joueur.score < 17:
+                print("le croupier tire une nouvelle carte")
+                print("--------------------------")  
+                croupier.distribue(1)
+                croupier.joueur.calcul_hand()
+                print("la main du croupier est :", croupier.joueur.hand)
+                print("--------------------------")  
+                print("le score du croupier est de :", croupier.joueur.score)
+                print("--------------------------")  
+                
+            
+            if croupier.joueur.score <= 21:
+                var = {croupier.joueur.score: "le croupier", joueur.score:"le joueur"}
+                print("{} remporte la partie, avec un score de {}".format(var.get(max(var)),max(var)))
+                print("--------------------------")  
+            else:
+                var = {croupier.joueur.score: "le croupier", joueur.score:"le joueur"}
+                print("{} remporte la partie, avec un score de {}".format(var.get(min(var)),min(var)))
+                print("--------------------------")  
+
+        while choix_carte_supp == "oui":
+
+            croupier.distribue(1, joueur)
+            joueur.calcul_hand(joueur)
+            print("la main du joueur est :", joueur.hand)
+            print("le score du joueur est :", joueur.score)
+            if joueur.score > 21:
+                print("tu as perdu")
+                break
+            else:
+                choix_carte_supp =""
+                while choix_carte_supp != "oui" and choix_carte_supp != "non":
+                    choix_carte_supp = input("Recevoir une nouvelle carte ?:")
+                if choix_carte_supp == "non":
+
+                    print("la main du croupier est :", croupier.joueur.hand)
+                    print("--------------------------")  
+                    print("le score du croupier est de :", croupier.joueur.score)
+                    print("--------------------------")  
+
+                    while croupier.joueur.score < 17:
+                        print("le croupier tire une nouvelle carte")
+                        print("--------------------------")  
+                        croupier.distribue(1)
+                        croupier.joueur.calcul_hand()
+                        print("la main du croupier est :", croupier.joueur.hand)
+                        print("--------------------------")  
+                        print("le score du croupier est de :", croupier.joueur.score)
+                        print("--------------------------")  
+                        
+                    
+                    if croupier.joueur.score <= 21:
+                        var = {croupier.joueur.score: "le croupier", joueur.score:"le joueur"}
+                        print("{} remporte la partie, avec un score de {}".format(var.get(max(var)),max(var)))
+                        print("--------------------------")  
+                    else:
+                        var = {croupier.joueur.score: "le croupier", joueur.score:"le joueur"}
+                        print("{} remporte la partie, avec un score de {}".format(var.get(min(var)),min(var)))
+                        print("--------------------------")
+
+                        
+
+                                
+
+
+
+
+
+
+        #while choix_carte_supp == "oui" and joueur.score <=21:
+         #   croupier.distribue(1, joueur)
+          #  joueur.calcul_hand(joueur)
+           # print("la nouvelle main du joueur est", joueur.hand)
+
+                
+
+
+
+'''
         choix_carte_supp =""
         while choix_carte_supp != "oui" and choix_carte_supp != "non":
             choix_carte_supp = input("le joueur veut continuer ?:")
-            if choix_carte_supp == "oui":
+            while choix_carte_supp == "oui" and joueur.score <= 21:
                 croupier.distribue(1, joueur)
                 joueur.calcul_hand()
                 print("la nouvelle main du joueur est", joueur.hand)
                 print("--------------------------")
                 print("le nouveau score du joueur est", joueur.score)
-                while joueur.score < 21:
+                if joueur.score <= 21:
                     choix_carte_supp = input("le joueur veut continuer ?:")
-                    croupier.distribue(1, joueur)
-                    joueur.calcul_hand()
-                    print("la nouvelle main du joueur est :", joueur.hand)
-                    print("--------------------------")
-                    print("le nouveau score du joueur est :", joueur.score)
-            elif choix_carte_supp == "non":
+            print(" Dommage, Vous avez dépassé 21!!")
+
+                                                                                                                               
                 print("on tire les mains du croupier pour comparer :")
+            while croupier.joueur.score < 17:
                 croupier.distribue(1)
-                print("la main du croupier est :", croupier.joueur.hand)
                 croupier.joueur.calcul_hand()
+                print("la nouvelle main du croupier est :", croupier.joueur.hand)
+                print("--------------------------")
                 print("le score du croupier est :", croupier.joueur.score)
-            choix_recommencer = input("le joueur veut rejouer ?:")
+            if 17 <= croupier.joueur.score <= 21 :
+                if joueur.score > croupier.joueur.score:
+                    print("le joueur remporte la partie")
+                elif joueur.score < croupier.joueur.score:
+                    print("le croupier remporte la partie")
+            else:
+                print("le croupier à dépassé 21, le joueur remporte la partie")
+            choix_recommencer = input("voulez vous rejouer ?:")
     print("Merci et à bientôt")
                 # comparaison des mains..
-               
+     
         #choix_carte_supp = choix_carte_supp[0].lower()
 
 
 
         #print(croupier.joueur.score)
+'''
--- joueur.py
+++ joueur.py
@@ -5,20 +5,31 @@ class Joueur:
         self.score=0
         self.name=name
 
-    def calcul_hand(self) :
+    def calcul_hand(self, name="croupier"):
         self.score = 0
         for i in self.hand[0:]:
             
             if i[0] == 'valet' or i[0] == 'roi' or i[0] == 'dame':
                 self.score += 10
-            elif i[0] == 'as':
+
+            elif i[0] == 'as' and name != "croupier":
                 choix = 0
-                while choix !="1" and choix != "11" :
+
+                while choix != "1" and choix != "11" :
                     choix = input("choisir 1 ou 11: ")
 
                 self.score += int(choix)
-                
+
+            elif i[0] == 'as' and name == "croupier":
+
+                if self.score <= 10:
+                    self.score += 10
+                else:
+                    self.score += 1
+
             else :
                 self.score += int(i[0])
 
-	
+
+
+
